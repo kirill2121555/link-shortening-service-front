4c1bf16393896da79ce4023b198d79c239b90162
kirill2121555 <kirill2121555@gmail.com>
2022-08-11 16:23:01 +0300

    fix

--- my-app/src/Component/http/http.jsx
+++ my-app/src/Component/http/http.jsx
@@ -1,16 +1,16 @@
 import { $host } from "."
 
 export const createlink = async (link) => {
-    const {data} = await $host.post('api/createlink', { link })
+    const {data} = await $host.post('/createlink', { link })
     return {data}
 }
 
 export const createcastomlink = async (link, textlink) => {
-    const {data} = await $host.post('api/createcastomlink',{link, textlink})
+    const {data} = await $host.post('/createcastomlink',{link, textlink})
     return {data}
 }
 
 export const getStatistics = async (link) => {
-    const { data } = await $host.post('api/linkstatistics', { link })
+    const { data } = await $host.post('/linkstatistics', { link })
     return {data}
 }
--- my-app/src/Component/http/index.js
+++ my-app/src/Component/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const $host = axios.create({
-    baseURL: 'http://localhost:5000/'
+    baseURL: 'https://link-shortening-service.herokuapp.com/'
 })
 
 export {
